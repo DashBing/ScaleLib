c611717d8c8ef207eb5f0461312e78590ba61507
DashBing <mcbbkf@outlook.com>
2024-06-04 22:53:52 +0800

    finish a example

--- example/test.cpp
+++ example/test.cpp
@@ -1,4 +1,3 @@
-#include <vector>
 #include "../src/scalelib.h"
 
 using namespace std;
@@ -9,7 +8,16 @@ int main(){
         {scale_null, 1},
         {G3, 2}, {E3, 1}, {G3, 1}, {C4, 3}, {scale_null, 1},
         {A3, 2}, {C4, 1}, {A3, 1}, {G3, 3}, {scale_null, 1},
-        {G3, 2}, {C3, 1}, {D3, 1}, {E3, 3}, {D3, 1}, {C3, 1}, {D3, 4}, {scale_null, 1}
+        {G3, 2}, {C3, 1}, {D3, 1}, {E3, 3}, {D3, 1}, {C3, 1}, {D3, 4}, {scale_null, 1},
+
+        {G3, 2}, {E3, 1}, {G3, 1}, {C4, 3}, {B3, 1}, {A3, 2}, {C4, 2}, {G3, 3}, {scale_null, 1},
+        {G3, 2}, {D3, 1}, {E3, 1}, {F3, 3}, {B2, 1}, {C3, 4}, {scale_null, 1},
+
+        {A3, 2}, {C4, 2}, {C4, 3}, {B3, 2}, {A3, 1}, {B3, 1}, {C4, 3}, {scale_null, 1},
+        {A3, 1}, {B3, 1}, {C4, 1}, {A3, 1}, {A3, 1}, {G3, 1}, {E3, 1}, {C3, 1}, {D3, 4}, {scale_null, 1},
+
+        {G3, 2}, {E3, 1}, {G3, 1}, {C4, 3}, {B3, 1}, {A3, 2}, {C4, 2}, {G3, 3}, {scale_null, 1},
+        {G3, 2}, {D3, 1}, {E3, 1}, {F3, 3}, {B2, 1}, {C3, 4}, {scale_null, 1}
     };
     music_play_4long(music);
     return 0;
