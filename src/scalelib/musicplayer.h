31520955c9a338258a3f1dce8b3068a1434d668e
DashBing <mcbbkf@outlook.com>
2024-06-15 20:55:31 +0800

    add higher music player function

--- src/scalelib/musicplayer.h
+++ src/scalelib/musicplayer.h
@@ -5,6 +5,7 @@
 #include "scale.h"
 
 #ifdef __cplusplus
+#include <cmath>
 #include <vector>
 
 namespace scalelib::musicplayer{
@@ -14,17 +15,27 @@ struct music_unit{
     unsigned int time;
 };
 
+struct music_unit_high{
+    scalelib::scale::scales_id scale;
+    unsigned int note_type;
+    double note_times;
+};
+
 #ifdef WIN32
+template<unsigned int whole_note_length=2000>
+void music_play_high(std::vector<music_unit_high> music){
+    for(auto i : music){
+        scalelib::scale::scale_beep(i.scale, floor(whole_note_length/i.note_type*i.note_times));
+    }
+}
+
 template<unsigned int beat_length=1>
 void music_play(std::vector<music_unit> music){
     for(auto i : music){
         scalelib::scale::scale_beep(i.scale, i.time*beat_length);
     }
 }
-/*template<unsigned int beat_length=500>
-void music_play_n(std::vector<music_unit> music){
-    music_play<beat_length>(music);
-}*/
+
 void music_play_low(std::vector<music_unit> music){
     for(auto i : music){
         scalelib::scale::scale_beep(i.scale, i.time);
@@ -35,6 +46,7 @@ void music_play_low(std::vector<music_unit> music){
 }
 #else
 
+#include <math.h>
 #include <stdlib.h>
 
 struct music_unit{
@@ -42,12 +54,32 @@ struct music_unit{
     unsigned int time;
 };
 
+struct music_unit_high{
+    scales_id scale;
+    unsigned int note_type;
+    double note_times;
+};
+
 struct music_table{
     struct music_unit * adress;
     unsigned long long length;
 }
 
+struct music_table_high{
+    struct music_unit_high * adress;
+    unsigned long long length;
+}
+
 #ifdef WIN32
+
+unsigned int whole_note_length=2000;
+
+void music_play_high(struct music_table_high music){
+    for(unsigned long long i = 0; i<music.length; i++){
+        scale_beep(music.adress[i].scale, floor(whole_note_length/music.adress[i].note_type*note_times));
+    }
+}
+
 void music_play(struct music_table music){
     for(unsigned long long i = 0; i<music.length; i++){
         scale_beep(music.adress[i].scale, music.adress[i].time);
